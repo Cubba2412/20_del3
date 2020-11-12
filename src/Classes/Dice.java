e6e2ea0144474f4b5297114fe6f4594f7e21a367
Muhammad Bangash <s092512@student.dtu.dk>
2020-11-12 11:21:56 +0100

    Renamed class to Dice.

--- src/Classes/Dice.java
+++ src/Classes/Dice.java
@@ -2,18 +2,18 @@ package Classes;
 
 import java.util.Random;
 
-public class Terning {
+public class Dice {
 
     private Random random = new Random();
 
-    public int generateRandomDiceValue(){
+    private int generateRandomDiceValue(){
         int diceMinimumValue = 1;
         int diceMaximumValue = 6;
         return diceMinimumValue + random.nextInt((diceMaximumValue - diceMinimumValue) + 1);
     }
 
-    public int kast()
+    public int roll()
     {
-        return generateRandomDiceValue() + generateRandomDiceValue();
+        return generateRandomDiceValue();
     }
 }
