8e65e400980b7ad9ce178ff421cabe94be138387
Muhammad Bangash <s092512@student.dtu.dk>
2020-11-12 11:27:17 +0100

    Added basic Player

---
+++ src/Classes/Player.java
@@ -0,0 +1,14 @@
+package Classes;
+
+public class Player {
+
+    private String navn;
+
+    public Player(String navn) {
+        this.navn = navn;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+}
