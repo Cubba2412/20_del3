a3f4f8eaa82baa061e18a52e33d36bc4cde3dbfb
Cubba2412 <thomasbf@live.dk>
2020-11-16 17:57:58 +0100

    Basic Gui initialized

--- src/main/java/Classes/Main.java
+++ src/main/java/Classes/Main.java
@@ -1,9 +1,12 @@
 package Classes;
 
+import gui_main.GUI;
+
 public class Main {
 
     public static void main(String[] args){
         Game game = new Game();
+        GUI gui = new GUI();
         game.start();
     }
 
