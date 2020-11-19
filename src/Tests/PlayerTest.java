54b85bac233aed0584db65f0cd4e980b25f8c4e7
Rivano28 <52214279+Rivano28@users.noreply.github.com>
2020-11-19 20:25:27 +0100

    Negativ and positive test for age and name

---
+++ src/Tests/PlayerTest.java
@@ -0,0 +1,38 @@
+package Tests;
+
+import Classes.Player;
+import Classes.PlayerFigureType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    private Player player;
+    @Before
+    public void init() {
+        player = new Player("Test name", 18, PlayerFigureType.Hunden, 10);
+    }
+    //positiv test method
+    @Test
+    public void testName() {
+        assertEquals("Test name", player.getName());
+    }
+    //NEgativ test method
+    @Test
+    public void testNameNegative() {
+        assertNotEquals("Test not name", player.getName());
+    }
+    //positive test for age
+    @Test
+    public void testAge(){
+        assertEquals(18, player.getAge());
+    }
+    //negative test for age
+    @Test
+    public void testAgeNegative(){
+        assertNotEquals(10, player.getAge());
+    }
+
+
+}
